adda58e45aeda7ff5fa66a8429646b20adb59ebc
JamesBolon <jamesdbolon@gmail.com>
2024-01-26 14:50:20 -0700

    Update Week3.py

--- Week3.py
+++ Week3.py
@@ -105,3 +105,32 @@ def element_in_dict(n, x):
         end_time = time.time()
         total_time = end_time - start_time
         return total_time
+
+
+n = 10000000
+n2 = 10000
+
+my_insert_list = [ ]
+dummy_list = [ ]
+
+for i in range(0, n, n2):
+    for i in range(5):
+        dummy_list.append(insert_beginning_list(n))
+
+    sum = 0
+    for i in range(5):
+        sum = sum + dummy_list[i]
+    sum / len(dummy_list)
+
+    my_insert_list.append(sum)
+
+
+file_path = "my_list.csv"
+
+# Open the file in 'w' mode with newline='' to prevent extra newline characters
+with open(file_path, 'w', newline='') as csv_file:
+    # Create a CSV writer
+    csv_writer = csv.writer(csv_file)
+
+    # Write the list as a row in the CSV file
+    csv_writer.writerow(my_insert_list)
