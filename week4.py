2db53a15b3fc4006806c2bdd9e423e637fb0acb6
JamesBolon <jamesdbolon@gmail.com>
2024-01-29 15:12:37 -0700

    Update week4.py

--- week4.py
+++ week4.py
@@ -1,3 +1,14 @@
 class Stack():
+    def __init__(self):
+        self.__index = [ ]
     def push(self, item):
         self.item = item
+        self.__index.append(self.item)
+    def pop(self):
+        try:
+            self.__index.pop()
+        except:
+            return None
+    
+
+        
