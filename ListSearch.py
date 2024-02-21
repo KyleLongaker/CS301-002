2838947fcb9633aa5b98158d31cc03d55b57df93
Kjuhl <157651346+Kjuhl@users.noreply.github.com>
2024-02-20 18:51:17 -0700

    Create ListSearch.py

---
+++ ListSearch.py
@@ -0,0 +1,46 @@
+class HashTable():
+    def __init__(self):
+      self.stack = []
+    def hashlist(self,length):
+      
+    def hashfunction(self,item):
+
+    def put(self,item):
+
+    def contains(self,item):
+
+    def items():
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
