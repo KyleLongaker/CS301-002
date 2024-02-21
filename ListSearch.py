dc350aa71bf6ed30df8d2158181e8671d25e4a5e
Kjuhl <157651346+Kjuhl@users.noreply.github.com>
2024-02-20 20:23:13 -0700

    Update ListSearch.py

--- ListSearch.py
+++ ListSearch.py
@@ -1,12 +1,24 @@
 class HashTable():
     def __init__(self):
       self.stack = []
-    def hashlist(self,length):
-      
-    def hashfunction(self,item):
-
-    def put(self,item):
-
+      self.size = 1000
+      self.slots = [None] * self.size
+      self.data = [None] * self.size
+        
+    def hash_function(self, key):
+      return key%self.size
+        
+    def put(self, key):
+        insert_pos = self.hash_function(key)
+    if self.slots[insert_pos] == None:
+        self.slots[insert_pos] = key
+    else:
+        insert_pos = self.rehash(insert_pos)
+        while self.slots[insert_pos] != None and self.slots[insert_pos] != key:
+            insert_pos = self.rehash(insert_pos)
+        if self.slots[insert_pos] == None:
+            self.slots[insert_pos] = key
+            
     def contains(self,item):
 
     def items():
