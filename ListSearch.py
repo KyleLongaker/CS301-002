7e14686e9586b2c4a3cc2bad0d53fa1cfe9bc7a9
Kjuhl <157651346+Kjuhl@users.noreply.github.com>
2024-02-22 13:13:41 -0700

    Update ListSearch.py

--- ListSearch.py
+++ ListSearch.py
@@ -1,29 +1,36 @@
+#Katelyn 
 class HashTable():
     def __init__(self):
-      self.stack = []
-      self.size = 1000
-      self.slots = [None] * self.size
-      self.data = [None] * self.size
-        
+          self.stack = []
+          self.size = 1000
+          self.slots = [None] * self.size
+          self.data = [None] * self.size
+
+    def put(self, key, data):
+            hash_value = self.hash_function(key, len(self.slots))
+        if self.slots[hash_value] is None:
+            self.slots[hash_value] = key
+            self.data[hash_value] = data
+        else:
+           return None
+
     def hash_function(self, key):
-      return key%self.size
+        return key%self.size
         
-    def put(self, key):
-        insert_pos = self.hash_function(key)
-    if self.slots[insert_pos] == None:
-        self.slots[insert_pos] = key
-    else:
-        insert_pos = self.rehash(insert_pos)
-        while self.slots[insert_pos] != None and self.slots[insert_pos] != key:
-            insert_pos = self.rehash(insert_pos)
-        if self.slots[insert_pos] == None:
-            self.slots[insert_pos] = key
-            
     def contains(self,item):
-
-    def items():
+        if item == key:
+             return True 
+             return (old_hash + 1) % size
+        else:
+             return False
+            
+    def items(self,key):
+        return self.get(key)
+        
       
-
+#Katelyn 
+In order to convert our hashlist into a dictionary we can create two lists, one for keys and one for values.
+We can then zip those lists together in order to get key value pairs and then convert those lists into a dictionary. 
 
 
 
