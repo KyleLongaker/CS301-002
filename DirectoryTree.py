79718f42d0be33295ee61082fa9c08e30dde9c90
Kjuhl <157651346+Kjuhl@users.noreply.github.com>
2024-03-15 11:32:08 -0600

    Create DirectoryTree.py

---
+++ DirectoryTree.py
@@ -0,0 +1,41 @@
+## Group Members: Katelyn Juhl, Kyle Longaker
+
+## Katelyn Juhl 
+class BinaryTree:
+    def __init__(self, root_obj):
+        self.key = root_obj
+        self.left_child = None
+        self.right_child = None]
+
+## Katelyn Juhl
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
