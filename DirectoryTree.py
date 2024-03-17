1028e6b8f617c73b0f775337a9b0da8cf6869e10
Kjuhl <157651346+Kjuhl@users.noreply.github.com>
2024-03-17 13:29:37 -0600

    Update DirectoryTree.py

--- DirectoryTree.py
+++ DirectoryTree.py
@@ -1,12 +1,10 @@
 ## Group Members: Katelyn Juhl, Kyle Longaker
 
 ## Katelyn Juhl 
-class DictionaryTree:
+class DirectoryTree:
     def __init__(self, root_obj):
         self.key = root_obj
-        self.left_child = None
-        self.right_child = None]
-
+        
 ## Katelyn Juhl
 
 
